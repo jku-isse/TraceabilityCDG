3114e5a493b8e11eb0a3df6ca64eafdbf1b1eb13
mouna2 <mounahammoudi2@gmail.com>
2020-11-03 20:18:35 +0100

    cc

--- TraceTool/src/mainPackage/CSV.java
+++ TraceTool/src/mainPackage/CSV.java
@@ -46,6 +46,7 @@ public class CSV {
     		+ "CalleesCalleesT,CalleesCalleesN,CalleesCalleesU,CompleteCallersCallees,"
     		+ "classGold"
     		
+    		
     		; 
     
     static String headersAtLeastOneInstance="gold,Program,MethodType,"
